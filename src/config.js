6c37547f732531e9cb28efb5fc90a26a28c28d82
xeonsim <byeonggeonsim@gmail.com>
2022-06-06 13:59:15 +0900

    update

--- src/config.js
+++ src/config.js
@@ -1,16 +1,37 @@
-// General metadata
+// 필수 설정 사항 
+//===================================================================
+//===================================================================
 const namePrefix = "TestCollection";
+// 컬렉션 이름입니다.
 const description = "Remember to replace this description";
+// 컬렉션에 대한 명입니다.
 const projectId = 3;
+// 위에서 확인한 프로젝트 아이디를 입력합니다.
 const _edtionSize = 30;
+// 해당 컬렉션의 작품 수 입니다. 해당 수 만큼 작품이 생성됩니다.
+//===================================================================
+//===================================================================
 
+// 선택 사항 
+//===================================================================
+//===================================================================
 const useBaseIpfs = false;
-const ipfsCID = "ferfagtntaferedwafrb";
+// Ipfs에 직접 사진 파일을 올릴지 설정합니다.
+const ipfsCID = "QmQU1DXv8nWoWhkqLwPpZSEenEAnPtRFypcDjAopYRq7Eg";
+// 사진 파일이 담긴 Ipfs 폴더 CID 입니다. 
 const baseIpfsUrl = `ipfs://${ipfsCID}`;
+//따로 설정하지 않으셔도 됩니다.
+//===================================================================
+//===================================================================
 
+
+// 필수 설정 사항 
+//===================================================================
+//===================================================================
 const layerConfigurations = [
   {
     growEditionSizeTo: _edtionSize,
+    // layersOrder에 레이어 폴더 순서대로 입력합니다.
     layersOrder: [
       { name: "Background" },
       { name: "Eyeball" },
@@ -23,8 +44,11 @@ const layerConfigurations = [
   },
 ];
 
+// format을 설정합니다.
 const format = {
+  //넓이 입니다.
   width: 512,
+  //높이 입니다.
   height: 512,
   smoothing: false,
 };
@@ -33,11 +57,17 @@ const pixelFormat = {
   ratio: 10 / 128,
 };
 
+// 추가 메타데이터를 설정합니다. 
 const extraMetadata = {
+//설정하지 않을 시 아래 내용을 지워주세
   external_url: "https://example.com",
   youtube_url: "https://youtube.com",
   creator: "ArtKonnection",
+//여기 까지 입니다.
 };
+//===================================================================
+//===================================================================
+
 
 const rarityDelimiter = "#";
 
